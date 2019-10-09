585b555ae1a8154b0e9d56636e89f2fc5928ac10
Ehsan Fazeli <fa.ehsan@gmail.com>
2019-10-09 15:56:21 +0330

    Update index.py

--- src/index.py
+++ src/index.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/login', methods=['GET', 'POST'])
 def hello_world():
-	if request.form['username'] == 'hasan' and request.form['password'] == 'password':
+	if request.form['username'] == 'username' and request.form['password'] == 'password':
 		return dumps({'msg': 'successful login'}), 200
 	return dumps({'msg': 'credentials don\'t match'}), 401
